d4cae441eb804813c73e12f1e57209561775c706
DaniBacheva <y.bacheva@abv.bg>
2023-09-17 20:30:52 +0300

    Add cubeManager with create logic

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,13 +1,29 @@
 const router = require('express').Router();
+const cubeManager = require('../managers/cubeManager');
 
 //path = /cubes/create =>
-router.get('/create', (req,res) => {
+router.get('/create', (req, res) => {
+console.log(cubeManager.getAll())
+
     res.render('create');
 })
 
-router.post('/create', (req,res)=> {
-    console.log(req.body);
-res.redirect('/')
+router.post('/create', (req, res) => {
+    const {
+        name,
+        difficaltyLevel,
+        imageUrl,
+        description,
+    } = req.body
+
+    cubeManager.create ({
+        name,
+        difficaltyLevel: Number(difficaltyLevel),
+        imageUrl,
+        description, 
+    })
+
+      res.redirect('/')
 })
 
 
