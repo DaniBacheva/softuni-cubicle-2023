8ab536d676d96ff788028626945b9be20d914c7b
DaniBacheva <y.bacheva@abv.bg>
2023-09-22 16:28:05 +0300

    Mongoose setup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require ("express");
 
 const expressConfig = require('./config/expressConfig');
 const hadlebarsConfig = require('./config/handlebarsConfig');
+const dbConnect = require ('./config/dbConfig');
 const routes = require('./routes')
 
 const app = express();
@@ -10,6 +11,10 @@ const PORT = 5500;
 
 expressConfig(app);
 hadlebarsConfig(app);
+
+dbConnect()
+.then(() => console.log('DB connected successfully'))
+.catch(err => console.log('DB error:', err));
 
 app.use(routes);
 
