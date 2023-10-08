136968055327426f93fa6295a4e807d9e101a26b
DaniBacheva <y.bacheva@abv.bg>
2023-10-08 21:31:37 +0300

    Save user with match password and repeatPassword

---
+++ src/models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require ('mongoose');
+
+const userSchema = new mongoose.Schema ({
+    username: String,
+    password: String,
+});
+
+userSchema.virtual('repeatPassword')
+   .set(function(value) {
+    if (value !== this.password) {
+        throw new mongoose.MongooseError('Password Mismatch');
+    }
+   });
+   
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
